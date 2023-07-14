e61d9b7fe4f13ed76f865365dffbfd070b0f81de
Wilson <wilsnyeh@gmail.com>
2023-07-14 10:51:24 -0700

    have mui modal working, need animal details

--- src/AnimalTableDataMUI.jsx
+++ src/AnimalTableDataMUI.jsx
@@ -1,92 +1,154 @@
-import "./App.css"; 
-import pawvector from './assets/paw-print-vector-icon.jpg';
-import ReactModal from "react-modal";
-import { DataGrid, GridToolbar, gridFilteredSortedRowEntriesSelector} from '@mui/x-data-grid'
+import "./App.css";
+import pawvector from "./assets/paw-print-vector-icon.jpg";
+// import ReactModal from "react-modal";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import React from "react";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box"
+import Typography from "@mui/material/Typography"
 
+export default function AnimalTableDataMUI({
+  searchContent,
+  setSearchContent,
+  setSelectedAnimalDetail,
+  selectedAnimalDetail,
+  setIsModalOpen,
+  isModalOpen,
+}) {
+  const handleRowClick = (detail) => {
+    // setSelectedAnimalDetail(detail);
+    setIsModalOpen(true);
+  }
 
-export default function AnimalTableDataMUI({ searchContent, setSearchContent, setSelectedAnimalDetail,selectedAnimalDetail,  setIsModalOpen, isModalOpen }) {
-  
-  // const handleRowClick = (detail) => {
-  //   setSelectedAnimalDetail(detail);
-  //   setIsModalOpen(true);
-  // }
+  const handleCloseModal = () => {
+    setIsModalOpen(false)
+  }
 
-  // const handleCloseModal = () => {
-  //   setIsModalOpen(false)
-  // }
-
-  // const handleDeleteAnimal = () => {
-  //   deleteOrder.call()
-  //   getOrders.refetch()
-
-  // }
 
   const handleAnimalDelete = (id) => {
-    const newList = searchContent.filter((item, index) => index !== id)
-    setSearchContent(newList)
-  }
-  const rows = 
-    searchContent && searchContent.map((x, i) => {
-      return (
-        {id: i , animalName: `${x.name}`, type: `${x.type}`, animalBreed: `${x.breed} ${x.breed2}`, animalLocation: `${x.city} ${x.state}`, 
-        photos: `${x.photo}`
-      })
+    const newList = searchContent.filter(( listitem, index) => index !== id);
+    setSearchContent(newList);
+  };
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+  // const handleAnimalDetails =(id) => {
+
+  // }
+  const rows =
+    searchContent &&
+    searchContent.map((x, i) => {
+      return {
+        id: i,
+        animalName: `${x.name}`,
+        type: `${x.type}`,
+        animalBreed: `${x.breed} ${x.breed2}`,
+        animalLocation: `${x.city} ${x.state}`,
+        photos: `${x.photo}`,
+      };
     });
 
-
   const columns = [
-    { field: 'animalName', headerName: 'Animal Name', width: 150 },
-    { field: 'type', headerName: 'Type', width: 150 },
-    { field: 'animalBreed', headerName: 'Animal Breed', width: 300,
-    renderCell: (params) => {
-      if (!params.value) {
-        return ''
-      }
-    }
-   },
-    { field: 'animalLocation', headerName: 'Animal Location', width: 150 },
-    { field: 'photos', headerName: 'Photos', width: 150,
-    renderCell: (params) => {
-      if (params.value === 'null'){
-        return <img src={pawvector} width='relative' height='100' alt='vector of a paw'/>
-      }
-      return <img src={params.value} width='relative' height='100' alt="some real animals" />
-    }
+    { field: "animalName", headerName: "Animal Name", width: 150 },
+    { field: "type", headerName: "Type", width: 150 },
+    {
+      field: "animalBreed",
+      headerName: "Animal Breed",
+      width: 300,
+      renderCell: (params) => {
+        if (!params.value) {
+          return "";
+        }
+      },
     },
-    {field: 'delete',
-  headerName: 'Delete',
-sortable: false,
-width: 100,
-disableClickEventBubbling: true,
-renderCell: (params)=> {
-  return <button onClick={()=> handleAnimalDelete(params.row.id)}>Delete</button>
-}}
+    { field: "animalLocation", headerName: "Animal Location", width: 150 },
+    {
+      field: "photos",
+      headerName: "Photos",
+      width: 150,
+      renderCell: (params) => {
+        if (params.value === "null") {
+          return (
+            <img
+              src={pawvector}
+              width="relative"
+              height="100"
+              alt="vector of a paw"
+            />
+          );
+        }
+        return (
+          <img
+            src={params.value}
+            width="relative"
+            height="100"
+            alt="some real animals"
+          />
+        );
+      },
+    },
+    {field: 'animalDetails',
+  headerName:'Animal Details', 
+width:100,
+renderCell: (params) => {
+  return (
+    <button onClick={handleRowClick}>Details</button>
+  )
+}},
+    {
+      field: "delete",
+      headerName: "Delete",
+      sortable: false,
+      width: 100,
+      renderCell: (params) => {
+        return (
+          <button onClick={() => handleAnimalDelete(params.row.id)}>
+            Delete
+          </button>
+        );
+      },
+    },
   ];
   return (
     <>
-
-<div style={{ height: 500, width: '100%' }}>
-      <DataGrid 
-      rows={rows}
-      rowHeight={100}
-      slots={{toolbar: GridToolbar}}
-      slotProps={{
-        toolbar: {
-          showQuickFilter: true,
-          quickFilterProps:{debounceMs:500}
-        }
-      }}
-      columns={columns} 
-      />
-</div>
-</>
+      <div style={{ height: 500, width: "100%" }}>
+        <DataGrid
+          rows={rows}
+          rowHeight={100}
+          slots={{ toolbar: GridToolbar }}
+          slotProps={{
+            toolbar: {
+              showQuickFilter: true,
+              quickFilterProps: { debounceMs: 500 },
+            },
+          }}
+          columns={columns}
+        />
+      </div>
+      <Modal
+      open={isModalOpen}
+      onClose={handleCloseModal}>
+        <Box sx={style}>
+          <Typography>Hello</Typography>
+        </Box>
+      </Modal>
+    </>
   );
 }
 
-      {/* <button type='button' onClick={handleDeleteAnimal}>delete</button> */}
-    //div here
-    {/* 
+
+//div here
+{
+  /* 
       <ReactModal isOpen={isModalOpen} onRequestClose={handleCloseModal} ariaHideApp={false} className='Modal-Content' overlayClassName='Modal-Portal'>
         {selectedAnimalDetail &&  (
           <>
@@ -120,7 +182,8 @@ renderCell: (params)=> {
           </>
         )}
         <button className='Modal-Close' onClick={handleCloseModal}>❌</button>
-      </ReactModal> */}
+      </ReactModal> */
+}
 //     </>
 //   );
 // }
